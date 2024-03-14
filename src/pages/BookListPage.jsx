7016b40cb7ee8dffadc46414c2cb0e90f90621f5
Egbagba <egbagba02@gmail.com>
2024-03-14 17:56:43 +0000

    update booklistpage

--- src/pages/BookListPage.jsx
+++ src/pages/BookListPage.jsx
@@ -15,7 +15,7 @@ function BookListPage() {
   }, []);
 
   return (
-    <div className="flex flex-wrap justify-content space-between">
+    <div className="flex flex-wrap space-between">
       {books &&
         books.map((book) => (
           <div key={book._id}>
