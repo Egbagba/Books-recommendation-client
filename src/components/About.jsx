3914b9ae9b44c859df2d4132ea7421b9c0055966
Egbagba <egbagba02@gmail.com>
2024-03-15 11:54:26 +0000

    apply changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,7 +27,7 @@ function About() {
          </div>    
          </div> */
     return (
-        <div className='bg-[url("https://images.pexels.com/photos/20522243/pexels-photo-20522243/free-photo-of-vase-with-blossoms-on-books.jpeg?auto=compress&cs=tinysrgb&w=600")] bg-cover bg-center full-screen w-76'>
+        <div className='bg-[url("https://images.pexels.com/photos/256514/pexels-photo-256514.jpeg?auto=compress&cs=tinysrgb&w=600")] bg-cover bg-center full-screen w-76'>
 
             <div style={{ position: 'relative', minHeight: '80vh' }}>
                 <div
