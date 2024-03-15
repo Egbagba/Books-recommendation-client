10e092186e464c511802b78b43befe1101a5f4f9
Egbagba <egbagba02@gmail.com>
2024-03-15 11:31:52 +0000

    apply changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,7 +27,6 @@ function About() {
          </div>    
          </div> */
     return (
-
         <div className='bg-[url("https://images.pexels.com/photos/4219038/pexels-photo-4219038.jpeg?auto=compress&cs=tinysrgb&w=600")] bg-cover bg-center full-screen w-76'>
 
             <div style={{ position: 'relative', minHeight: '80vh' }}>
@@ -47,31 +46,35 @@ function About() {
                     <h3 className="text-stone-950 italic hover:not-italic text-2xl font-serif font-bold m-20 inline-block align-middle ">Welcome to our Book Recommendation Web Application!</h3>
                 </div>
                 <div>
-                <div className="break-all italic my-8 ml-4 mr-4 inline-block align-bottom font-serif mb-10 text-zinc-900  text-left text-xl semibold" >
-                    <p>Here, we are dedicated to creating an immersive literary
-                        experience tailored just for you. Our platform is designed
-                        to foster a love for reading by providing personalized book
-                        recommendations that align with your unique preferences.</p>
-                        
-                    <p>The About Page is a glimpse into our mission,values, and the dedicated team behind the scenes. Feel free to explore
-                        and connect with us as we embark on this literary adventure together.
-                        Happy reading!
-                    </p>
-                </div>
+                    <div className="break-all italic my-8 ml-4 mr-4 inline-block align-bottom font-serif mb-10 text-zinc-900  text-left text-xl semibold" >
+                        <p>Here, we are dedicated to creating an immersive literary
+                            experience tailored just for you. Our platform is designed
+                            to foster a love for reading by providing personalized book
+                            recommendations that align with your unique preferences.</p>
 
-                <div>
-                    <div className="space-x-6 mt-20 flex justify-between px-6">
-                        <a href={`${"https://github.com/Egbagba"}`}><button className="btn btn-outline btn-success">Ese Egbagba</button></a>
-                        <a href={`${"https://github.com/Andr3L4"}`}><button className="btn btn-outline btn-warning">Andre SR</button></a>
-                        <div className="mt-20 footer footer-center">
+                        <p>The About Page is a glimpse into our mission,values, and the dedicated team behind the scenes. Feel free to explore
+                            and connect with us as we embark on this literary adventure together.
+                            Happy reading!
+                        </p>
+                    </div>
+
+                    <div>
+                        <div className="mt-auto px-6 pb-10">
+                            <div className="space-x-6">
+                                <a href={`${"https://github.com/Egbagba"}`}><button className="btn btn-outline btn-success">Ese Egbagba</button></a>
+                                <a href={`${"https://github.com/Andr3L4"}`}><button className="btn btn-outline btn-warning">Andre SR</button></a>
+                            </div>
+                            <div className="mt-20 footer footer-center">
+                            </div>
                         </div>
                     </div>
-                </div>
                 </div>
             </div>
             < Footer />
         </div>
-    )
+    );
+
+
 }
 
 export default About;
